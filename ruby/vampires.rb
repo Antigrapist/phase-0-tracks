73a920d774af440689d2b07abb4bb9351514c716
Antigrapist <antigrapist@gmail.com>
2016-11-18 01:17:00 -0500

    fixs name check, still need to convert some user input to booleans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,22 +1,24 @@
-# puts "What is your name?"
-# full_name=gets.chomp
-# puts "How old are you?"
-# age=gets.chomp
-# puts "What year were you born?"
-# birth_year=gets.chomp
-# puts "Our company cafeteria serves garlic bread. Should we order some for you? (true, false)"
-# wants_garlic=gets.chomp
-# puts "Would you like to be enrolled in the company’s health insurance? (true, false)"
-# wants_insurance=gets.chomp
-full_name = "alex"
-age = "10"
-birth_year = "2004"
-wants_garlic = false
-wants_insurance = false
+puts "What is your name?"
+full_name=gets.chomp
+puts "How old are you?"
+age=gets.chomp.to_i
+puts "What year were you born?"
+birth_year=gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (true, false)"
+wants_garlic=gets.chomp
+puts "Would you like to be enrolled in the company’s health insurance? (true, false)"
+wants_insurance=gets.chomp
+
+#it's a lot faster to test using set variables rather than typing them out each time
+# full_name = "alex"
+# age = 10
+# birth_year = 2006
+# wants_garlic = true
+# wants_insurance = false
 
 is_vampire="Results inconclusive"
 
-if birth_year.to_i + age.to_i == 2016
+if birth_year + age == 2016
 	age_test=true
 else
 	age_test=false
@@ -29,15 +31,21 @@ case
 	when !age_test && (!wants_garlic || !wants_insurance)
 		is_vampire = "Probably a vampire"
 
-	when !age_test && !wants_garlic && !wants_insurance
+	when !age_test && (!wants_garlic && !wants_insurance)
 		is_vampire = "Almost certainly a vampire."
 end
 
-# when full_name.downcase ==  "drake cula" || "tu Fang"
-# 	is_vampire= "Definitely a vampire."
-# end
+#I tried to do the name comparsion in the case using 'when' but it didn't seem to work that way :/
+if full_name.downcase == "drake cula"
+ 	is_vampire = "Definitely a vampire."
+elsif full_name.downcase == "tu fang"
+	is_vampire = "Definitely a vampire."
+end
 
 puts is_vampire
-puts age_test
+puts full_name
+puts !age_test
 puts wants_garlic
 puts wants_insurance
+puts age
+puts birth_year
