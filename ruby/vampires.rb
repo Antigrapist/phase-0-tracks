5e43b99d71d9e8e2e260ccbd208fbf36a52abcc6
Antigrapist <antigrapist@gmail.com>
2016-11-17 21:58:40 -0500

    made first stab at vampire tests

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,8 +4,30 @@ puts "How old are you?"
 age=gets.chomp
 puts "What year were you born?"
 birth_year=gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (true, false)"
 wants_garlic=gets.chomp
-puts "Would you like to enroll in the company’s health insurance?"
+puts "Would you like to be enrolled in the company’s health insurance? (true, false)"
 wants_insurance=gets.chomp
 
+is_vampire="Results inconclusive"
+
+if birth_year + age == 2016
+	age_test=true
+else
+	age_test=false
+end 
+
+if age_test && (wants_garlic ||wants_insurance)
+	is_vampire="Probably not a vampire."
+end
+if !age_test && !(wants_garlic ||wants_insurance)
+	is_vampire="Probably a vampire"
+end
+if !age_test && !wants_garlic && !wants_insurance
+	is_vampire="Almost certainly a vampire."
+end
+#if full_name.downcase ==  "drake cula" || "tu Fang"
+#	is_vampire= "Definitely a vampire."
+#end
+
+puts is_vampire
