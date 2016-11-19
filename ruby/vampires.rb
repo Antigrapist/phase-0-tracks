34acf8a3b55dcfcc7eadc270dda36b5aeba76d77
Antigrapist <antigrapist@gmail.com>
2016-11-18 21:28:06 -0500

    vampire detection now functional

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,9 +4,9 @@ puts "How old are you?"
 age=gets.chomp.to_i
 puts "What year were you born?"
 birth_year=gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (true, false)"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y, n)"
 wants_garlic=gets.chomp
-puts "Would you like to be enrolled in the company’s health insurance? (true, false)"
+puts "Would you like to be enrolled in the company’s health insurance? (y, n)"
 wants_insurance=gets.chomp
 
 #it's a lot faster to test using set variables rather than typing them out each time
@@ -15,6 +15,18 @@ wants_insurance=gets.chomp
 # birth_year = 2006
 # wants_garlic = true
 # wants_insurance = false
+if wants_garlic == "y"
+	wants_garlic = true
+else
+	wants_garlic = false
+end
+
+if wants_insurance == "y"
+	wants_insurance = true
+else
+	wants_garlic = false
+end
+
 
 is_vampire="Results inconclusive"
 
@@ -28,11 +40,13 @@ case
 	when age_test && (wants_garlic || wants_insurance)
 		is_vampire = "Probably not a vampire."
 
+	when !age_test && !wants_garlic && !wants_insurance
+	is_vampire = "Almost certainly a vampire."
+
 	when !age_test && (!wants_garlic || !wants_insurance)
 		is_vampire = "Probably a vampire"
 
-	when !age_test && (!wants_garlic && !wants_insurance)
-		is_vampire = "Almost certainly a vampire."
+
 end
 
 #I tried to do the name comparsion in the case using 'when' but it didn't seem to work that way :/
@@ -44,7 +58,7 @@ end
 
 puts is_vampire
 puts full_name
-puts !age_test
+puts age_test
 puts wants_garlic
 puts wants_insurance
 puts age
