c77ac695946f22143916e85e964de4c5dbcdade4
Antigrapist <antigrapist@gmail.com>
2016-11-21 20:31:31 -0500

    Draws up basic pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+# Ask the user for a client detail 
+# Convert the output if needed and put the resulting value in the hash
+# Repeat for each detail (could use a loop for this ui part)
+# 
+# Print the hash 
+# 
+# Ask for hash updates
+# 	If yes, then ask for a key value and then ask for the corrected data. Finally print the corrected hash
+# 	If no, exit the program
+
