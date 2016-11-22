1c21e54f3d0812eca554f97f5e5b22fdd368ab48
Antigrapist <antigrapist@gmail.com>
2016-11-21 21:06:37 -0500

    Adds first question and minor changes to the plan.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,10 +1,16 @@
 # Ask the user for a client detail 
 # Convert the output if needed and put the resulting value in the hash
-# Repeat for each detail (could use a loop for this ui part)
+# Repeat for each detail 
 # 
 # Print the hash 
 # 
 # Ask for hash updates
 # 	If yes, then ask for a key value and then ask for the corrected data. Finally print the corrected hash
-# 	If no, exit the program
+# 	If no changes, exit the program
 
+client_details = { }
+
+puts "What is the client's name?"
+client_details[:client_name] =gets.chomp
+
+p client_details
