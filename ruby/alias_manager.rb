792e92fea5b306208065b9ea5e8f29d9b3ccb826
Antigrapist <antigrapist@gmail.com>
2016-11-23 21:56:26 -0500

    Peudocode written and the basic outlines of the three methods started

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,33 @@
+# Get the name
+# split the first and last names into different variables. Use .downcase in here to avoid capital letters
+# For each word, call a method that swaps the vowels.
+	#Iterate through the string looking for vowels and changing them 
+	#Could hard code all five vowels
+#Now call a method that swaps the consonents 
+	# Iterate through the string, if a consonent, look up it's place in the sting of consonents add 1 to that number aand then return that letter
+	# hard code z > b
+#
+
+def next_vowel(name)
+	counter = 0
+	while counter < word.length
+
+	end	
+end
+
+def next_consonent(name)
+	counter = 0
+	while counter < word.length
+
+	end	
+end
+
+def fake_name_maker(name)
+	next_vowel(name)
+	next_consonent(name)
+	name.split(' ')
+	fake_name = name[1] +" "+name[0] 
+	fake_name
+end
+
+
