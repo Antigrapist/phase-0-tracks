daef3da220044bc161f09cb25f07c8daa2769d73
Antigrapist <antigrapist@gmail.com>
2016-11-27 18:53:11 -0500

    initial work added to next_consonent

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,10 @@
 # Get the name
-# split the first and last names into different variables. Use .downcase in here to avoid capital letters
+# split the first and last names into different variables. Use .downcase to avoid capital letters
 # For each name, call a method that swaps the vowels.
 	#Iterate through the string looking for vowels and changing them 
 	#Could hard code all five vowels
 #Now call a method that swaps the consonents 
-	# Iterate through the string, if a consonent, look up it's place in the sting of consonents add 1 to that number aand then return that letter
+	# Iterate through the string, if a consonent, look up it's place in the string of consonents add 1 to that number and then return that letter
 	# hard code z > b
 #
 
@@ -31,14 +31,22 @@ def next_vowel(name)
 	name
 end
 
-def next_consonent(name)
-	counter = 0
-	while counter < name.length
-
-
-		counter += 1
-	end	
-end
+# def next_consonent(name)
+# 	counter = 0
+# 	while counter < name.length
+# 		if name[counter] == "d" || "h" || "n" || "t"
+# 			name[counter]= name[counter].next.next
+# 		elsif name[counter] == "z"
+# 			name[counter] == "b"
+# 		elsif name[counter] == "a" || "e" || "i" || "o" || "u"
+				
+# 		else
+# 			name[counter]= name[counter].next
+# 		end
+# 		counter += 1	
+# 	end
+# 	name
+# end
 
 def fake_name_maker(name)
 	next_vowel(name.downcase)
