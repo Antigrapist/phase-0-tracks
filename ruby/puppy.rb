5e8c232bf22b62a21ee2c24370f103969e152956
Antigrapist <antigrapist@gmail.com>
2016-11-28 21:45:38 -0500

    Adds puppy atributes

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Canidae 
+
+# CHARACTERISTICS ----------------
+# Cute: yes
+# Friendly: yes
+# Name: varies
+# Color: varies
+# Ears: 2
+# Energy: high
+
+# BEHAVIOR -----------------------
+# Run
+# Play
+# Sleep
+# Generate messes
