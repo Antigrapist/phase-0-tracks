767c4fd393da67d34926ac1691a2e6d43f89216c
Antigrapist <antigrapist@gmail.com>
2016-12-01 00:59:48 -0500

    Remade shout module and made two classes that include it

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,36 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	":) " + words + " :D"
+#   end
+
+# end
+
+# p Shout.yell_angrily("GRRR")
+# p Shout.yelling_happily("TRALALALALA")
+
+
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yelling_happily(words)
-  	":) " + words + " :D"
-  end
-
+	def yell(words)
+		puts "#{words.upcase}!!!"
+	end
 end
 
-p Shout.yell_angrily("GRRR")
-p Shout.yelling_happily("TRALALALALA")
+
+class Girl
+	include Shout
+end
+
+class Angry_Mob
+	include Shout
+end
+
+lucy = Girl.new
+lucy.yell("i hate eating carrots")
+
+rioters = Angry_Mob.new
+rioters.yell "rabble rabble rabble"
