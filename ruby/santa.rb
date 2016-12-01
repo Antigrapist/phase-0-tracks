c4a8381fd0f2417e2ce3ab4c1fdf3fb1da072763
Antigrapist <antigrapist@gmail.com>
2016-11-30 21:55:37 -0500

    refactored to use attr_reader and accessor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,12 @@
 class Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
 
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance ..."
 	end
 
@@ -17,16 +19,12 @@ class Santa
 	end
 
 	def celebrate_birthday
-		age +=1
+		@age +=1
 	end
 
 	def get_mad_at(bad_raindeer)
-		reindeer_ranking.delete_if { |x| x = bad_raindeer }
-		reindeer_ranking.insert(-1, bad_raindeer)
-	end
-
-	def gender=(new_gender)
-		@gender = new_gender
+		@reindeer_ranking.delete_if { |x| x == bad_raindeer }
+	 	@reindeer_ranking.insert(-1, bad_raindeer)
 	end
 
 end
@@ -38,4 +36,9 @@ santas = []
 
 santas << Santa.new("m", "old white dude")
 santas << Santa.new("f", "NA")
+
+santas[1].gender = ("kakuro")
+santas[0].get_mad_at("Dancer")
+santas[1].celebrate_birthday
+puts santas[1].age
 p santas
