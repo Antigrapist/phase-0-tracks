f868cf0420e90e17dbb70e64272f80ec06b3b383
Antigrapist <antigrapist@gmail.com>
2016-12-04 13:58:35 -0500

    made a method to take a word and output number of guesses

--- ruby/hangman_game/hangman.rb
+++ ruby/hangman_game/hangman.rb
@@ -1,7 +1,30 @@
 class Hangman
+	attr_accessor :puzzle_word
+	attr_accessor :guesses_remaining
+	attr_accessor :word_in_progress
+
+
+	def submit_puzzle_word(word)
+		@puzzle_word = word
+		@word_in_progress = []
+		@guesses_remaining = @puzzle_word.length
+
+		@puzzle_word.length.times do ||
+			@word_in_progress << "_"
+			
+		end
+		p @word_in_progress
+		@guesses_remaining
+	end
 
 
 
-
-	
 end
+
+
+
+
+#driver code
+puts "Please submit the word you want your opponent to guess"
+game = Hangman.new
+game.submit_puzzle_word("potato") #Need to add UI that uses gets.chomp
