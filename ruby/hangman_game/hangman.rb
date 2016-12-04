e63c0070aa5e71085ff33a66019f6addf1e30783
Antigrapist <antigrapist@gmail.com>
2016-12-04 16:46:14 -0500

    Working on submit_guess and building tests

--- ruby/hangman_game/hangman.rb
+++ ruby/hangman_game/hangman.rb
@@ -2,18 +2,37 @@ class Hangman
 	attr_accessor :puzzle_word
 	attr_accessor :guesses_remaining
 	attr_accessor :word_in_progress
+	attr_accessor :is_game_over
+
+	def initialize
+		@puzzle_word = ""
+		@guesses_remaining = 0
+		@is_game_over = false
+	end
+
 
 
 	def submit_puzzle_word(word)
-		@puzzle_word = word
+		@puzzle_word = word.downcase.split ""
 		@word_in_progress = []
+		@letters_guessed = []
 		@puzzle_word.length.times do 
 			@word_in_progress << "_"	
 		end
 		@guesses_remaining = @puzzle_word.length
 	end
 
+	def submit_guess(letter)
+		if letter.length > 1
+			return "One letter at a time please"
+		elsif @letters_guessed
 
+		else
+			
+
+		end
+
+	end
 
 end
 
@@ -21,6 +40,12 @@ end
 
 
 #driver code
-puts "Please submit the word you want your opponent to guess"
-game = Hangman.new
-game.submit_puzzle_word("potato") #Need to add UI that uses gets.chomp
+# puts "Please submit the word you want your opponent to guess"
+# game = Hangman.new
+# game.submit_puzzle_word("Potato") #Need to add UI that uses gets.chomp
+# puts "Now it's time to guess!" 
+# until @is_game_over
+# 	puts "Please submit a letter"
+
+	
+# end
--- ruby/hangman_game/hangman_rspec.rb
+++ ruby/hangman_game/hangman_rspec.rb
@@ -6,5 +6,9 @@ describe Hangman do
 	it "stores a word and outputs the number of guesses" do
 	expect(hangman.submit_puzzle_word("potato")).to eq 6
 	end 
+
+	it "Make a guess with multiple letters and have it return an error" do
+	expect(hangman.submit_guess("abc")).to eq "One letter at a time please"
+	end
 	
 end
