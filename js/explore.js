b91c8d3572e18dc4b812fbf33246557b949fc505
Antigrapist <antigrapist@gmail.com>
2016-12-06 16:12:10 -0500

    initial attempt at making the reversal loop

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+// have a counter that is equal to the length of the string
+// then take the letter at the counter's position and add it to a new variable
+// then reduce the counter by one and repeat until the counter is 0
+// 
+// 
+// 
+var original_string = "potatoes"
+var final_string = ""
+
+for (var i = original_string.length; i > 0; i-=1) {
+	final_string + charAt(i)
+} 
+console.log (final_string)
