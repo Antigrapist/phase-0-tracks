5635fbdca10a683ac49d961c38b6d60d39c2dc3a
Antigrapist <antigrapist@gmail.com>
2016-12-08 19:49:27 -0500

    adds first attempt at a random word generator

--- js/algos.js
+++ js/algos.js
@@ -11,5 +11,44 @@ function find_longest_word(word_array) {
 	}
 	return word_array ;
 }
+
+
+function find_common_key_value_pair(object1, object2) {
+
+var is_common_key_value_pair = false ;
+
+for (var key in object1) {
+	if (object2.hasOwnProperty(key) ){ 
+		if object1(key) == object2(key){
+			is_common_key_value_pair = true;
+		}
+
+	} 
+}
+return is_common_key_value_pair;
+}
+
+
+function generate_random_letters(number_of_words) {
+	var alphabet = "abcdefghijklmnopqurstuvwxyz";
+	var array_of_random_words = [];
+	for (var i = 0; i < number_of_words; i++) {
+		var random_word_length = Math.floor((Math.random() * 10) + 1);
+		var random_word = "";
+		for (var j=0; j < random_word_length; j++){
+			var random_charactor_index = math.floor((Math.random()* 26));
+			var letter = alphabet.substr(random_charactor_index);
+			random_word += letter;
+		}
+		array_of_random_words.push(random_word);
+	}
+	return array_of_random_words;
+}
+
+
+
+
+
+
 var words = ["rice","beans", "dinosaurj", "iuhesouhnifsnsiopfnsf", "d" , "a" , "cheese"] ;
 find_longest_word(words) ;
